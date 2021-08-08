eb068ef192b4067efe58e3b1d19fd661b1dba9f8
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2021-08-08 11:51:20 +0000

    Add background to the canvas element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,25 @@
 let canvas = document.getElementById('game-area')
 let ctx = canvas.getContext('2d')
+/*let background = document.getElementById('background')*/
 
 let seagullX = 200;
 let seagullY = 400;
 let speed = 7;
 let score = 0;
 
+let background = new Image()
+background.src = 'assets/images/Seafront_01.JPG'
+background.onload = function(){
+    ctx.drawImage(background,0,0);   
+}
+
 function gameLoop() {
+
     ctx.fillStyle = "green";
     ctx.fillRect(seagullX -50, seagullY -50, 50, 50)
 
     setTimeout (gameLoop, 1000/speed)
 }
 
-function background() {
-    //essentiallyclears screen
-}
-
 gameLoop()
 
-/*
-let hello = 'Hello'
-console.log(hello)
-*/
