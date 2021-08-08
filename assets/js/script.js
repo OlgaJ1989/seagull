2186f893ded7a4b0a64f640216586ea84a0e53b2
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2021-08-08 15:36:41 +0000

    Add jump functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,18 @@ let backgroundCount = 0;
 
 let seagullX = 200;
 let seagullY = 400;
-let speed = 7;
+let speed = 15;
 let score = 0;
+let acceleration = 0;
+let gravity = 2;
+let gullFloor = 400;
+let gullState = 0;
+let gullJump = 0;
+let keysPressed = {};
 
 //Load canvas background image
 let background = new Image()
-background.src = 'assets/images/Seafront_01.JPG'
+background.src = 'assets/images/seafront_03.jpg'
 background.onload = function(){
     ctx.drawImage(background,0,0);   
 }
@@ -24,16 +30,63 @@ function drawBackground() {
     ctx.drawImage(background,1000 - backgroundCount,0);
 } 
 
+function seagullJump() {
+    
+    if (gullState == 0 && gullJump == 1) {
+        acceleration = -30;
+        gullState = 1;
+        
+    }
+}
+
+function seagullLand() {
+    if (seagullY >= gullFloor) {
+        acceleration = 0;
+        gullState = 0;
+        seagullY = gullFloor;
+    }
+
+}
+
 //Main game loop
 function gameLoop() {
 
     drawBackground()
+    if (keysPressed[32] == true) {
+        gullJump =1;
+    } else if (keysPressed[32] == false) {
+        gullJump =0;
+    }
+    if (gullState == 1 ) {
+        acceleration += gravity;
+    } else if (gullState == 0) {
+        acceleration =0;
+    }
+    seagullY += acceleration;
+    let hello = gullJump
+    console.log(hello);
+    
+    seagullLand()
+    seagullJump()
     let seagull = new Image()
     seagull.src = 'assets/images/gullSmall.png'
     ctx.drawImage(seagull, seagullX -90, seagullY-90)
     
     setTimeout (gameLoop, 1000/speed)
 }
+
+window.onkeyup = function(e) { keysPressed[e.keyCode] = false }
+window.onkeydown = function(e) { keysPressed[e.keyCode] = true}
+
+
+
+  
+
+
+
+
+
+
 
 //Call main game loop
 gameLoop()
