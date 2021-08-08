681cb6fff8f34d0f0ef4e3f793f6f5f98a30d3cf
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2021-08-08 12:19:17 +0000

    Calculate background position and make it move across canvas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,33 @@
 let canvas = document.getElementById('game-area')
 let ctx = canvas.getContext('2d')
-/*let background = document.getElementById('background')*/
+let backgroundCount = 0;
 
 let seagullX = 200;
 let seagullY = 400;
 let speed = 7;
 let score = 0;
 
+//Load canvas background image
 let background = new Image()
 background.src = 'assets/images/Seafront_01.JPG'
 background.onload = function(){
     ctx.drawImage(background,0,0);   
 }
 
+//Calculate background position and display image 
+function drawBackground() {
+    backgroundCount +=4;
+    if (backgroundCount>=1000) {
+        backgroundCount -= 1000;
+    }
+    ctx.drawImage(background,0 - backgroundCount,0);
+    ctx.drawImage(background,1000 - backgroundCount,0);
+} 
+
+//Main game loop
 function gameLoop() {
 
+    drawBackground()
     let seagull = new Image()
     seagull.src = 'assets/images/gullSmall.png'
     ctx.drawImage(seagull, seagullX -90, seagullY-90)
@@ -22,5 +35,6 @@ function gameLoop() {
     setTimeout (gameLoop, 1000/speed)
 }
 
+//Call main game loop
 gameLoop()
 
